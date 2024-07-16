7b15f7604462e89a970725a6d272ac18c4076219
GiovanaJensen <oliveirasgiovana143@gmail.com>
2024-07-16 09:45:48 -0300

    add: style for guests modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import {MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2} from 'lucide-react';
+import {MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus} from 'lucide-react';
 import { useState } from 'react';
 
 export function App() {
@@ -15,6 +15,10 @@ export function App() {
 
   function openGuestModal(){
     setIsGuestModalOpen(true);
+  }
+
+  function closeGuestModal(){
+    setIsGuestModalOpen(false);
   }
 
   return (
@@ -72,8 +76,38 @@ export function App() {
 
       {isGuestModalOpen && (
         <div className='fixed insert-0 bg-black/60 flex items-center justify-center'>
-          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900'>
-            <h2>Selecionar convidados</h2>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className="flex items-center justify-between">
+              <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
+                <button type='button' onClick={closeGuestModal}>
+                  <X className='size-5 text-zinc-600'/>
+                </button>
+              </div>
+              <p className='text-sm text-zinc-400'>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
+            </div>
+            <div className='flex flex-wrap gap-2'>
+              <div className='px-2.5 py-1.5 bg-zinc-800 rounded-md flex items-center gap-2'>
+                <span className='text-zinc-300'>
+                jessica.white44@yahoo.com
+                </span>
+                <button type="button">
+                  <X className='size-4 text-zinc-400'/>
+                </button>
+              </div>
+            </div>
+            <div className='w-full h-px bg-zinc-800'/>
+            <form className='p-3 bg-zinc-950 border-zinc-800 rounded-lg flex items-center'>
+              <div className='px-2 flex items-center gap-2 flex-1'>
+                <AtSign className='size-5 text-zinc-400'/>
+                <input type="text" placeholder='Digite o e-mail do convidado' className='bg-transparent text-lg placeholder:text-zinc-400 outline-none flex-1'/>
+              </div>
+
+              <button className='bg-lime-300 text-lime-950 px-5 py-2 rounded-lg font-medium flex items-center gap-2 hover:bg-lime-400'>
+                Convidar
+                <Plus className='size-5'/>
+              </button>
+            </form>
           </div>
         </div>
       )}
